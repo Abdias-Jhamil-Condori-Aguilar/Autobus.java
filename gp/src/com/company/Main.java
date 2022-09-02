78b9217bc591b4ca1ab5d6826b8159e7279f2435
Abdias-Jhamil-Condori-Aguilar <condori.abdias.jhamil@gmail.com>
2022-09-02 10:43:59 -0400

    Practicaenclase

---
+++ gp/src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Persona{
+
+    public static void main(String[] args) {
+        public string nombre;
+               string primerApellido;
+        public string segundoApellido;
+        public Date fechaNacimiento;
+           string CI:
+	// write your code here
+    }
+}
---
+++ src/Modelo.java
@@ -0,0 +1,11 @@
+public class Modelo {
+    int capacidad;
+    double consumo;
+public Modelo(){
+
+}
+    public Modelo(int capacidad,double consumo) {
+        this.capacidad = capacidad;
+        this.consumo=consumo;
+    }
+}
